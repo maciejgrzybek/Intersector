c961aaecdaf75c18f9ff5c1788a1f7652f1a29b3
Maciej 'mac' Grzybek <mashester@gmail.com>
2012-05-10 20:55:31 +0200

    First Implementation of Figure intersection. Implementation basis on visitor design pattern. It's simplification of multimethod implementation with visitor.

---
+++ Figure.cpp
@@ -0,0 +1,107 @@
+#include "Figure.h"
+#include <cmath>
+
+SquareVisitor::SquareVisitor(const Square& square) : square_(square)
+{}
+
+CircleVisitor::CircleVisitor(const Circle& circle) : circle_(circle)
+{}
+
+bool SquareVisitor::visit(const Square& square) const
+{
+    double distanceX = square_.getDistanceX(square);
+    double distanceY = square_.getDistanceY(square);
+    double allowedDistance = square.getEdgeLength()/2 + square_.getEdgeLength()/2;
+    if((distanceX > allowedDistance) || (distanceY > allowedDistance))
+    {
+        return false;
+    }
+    return true;
+}
+
+bool SquareVisitor::visit(const Circle&) const
+{
+    throw IntersectionNotSupported();
+}
+
+bool CircleVisitor::visit(const Circle& circle) const
+{
+    double distance = circle_.getDistance(circle);
+    double allowedDistance = circle_.getRadius()/2 + circle.getRadius();
+    if(distance > allowedDistance)
+    {
+        return false;
+    }
+    return true;
+}
+
+bool CircleVisitor::visit(const Square&) const
+{
+    throw IntersectionNotSupported();
+}
+
+Figure::Figure(const Point& point) : centerPoint(point)
+{}
+
+double Figure::getDistance(const Figure& figure) const
+{
+    Point subPoint = figure.centerPoint - this->centerPoint;
+    return sqrt(pow(subPoint.x,2) + pow(subPoint.y,2));
+}
+
+Square::Square(const Point& center, unsigned int length) : Figure(center),edgeLength(length)
+{}
+
+Circle::Circle(const Point& center, unsigned int radius) : Figure(center),radius_(radius)
+{}
+
+double Square::getDistanceX(const Square& square) const
+{
+    return abs(this->centerPoint.x - square.centerPoint.x);
+}
+
+double Square::getDistanceY(const Square& square) const
+{
+    return abs(this->centerPoint.y - square.centerPoint.y);
+}
+
+
+bool Square::accept(IntersectVisitor& v) const
+{
+    return v.visit(*this);
+}
+
+bool Square::intersects(const Figure& figure) const
+{
+    SquareVisitor squareVisitor(*this);
+    return figure.accept(squareVisitor);
+}
+
+double Square::getEdgeLength() const
+{
+    return edgeLength;
+}
+
+double Circle::getRadius() const
+{
+    return radius_;
+}
+
+bool Circle::accept(IntersectVisitor& v) const
+{
+    return v.visit(*this);
+}
+
+bool Circle::intersects(const Figure& figure) const
+{
+    CircleVisitor circleVisitor(*this);
+    return figure.accept(circleVisitor);
+}
+
+Point Point::operator-(const Point& point) const
+{
+    Point result;
+    result.x = this->x - point.x;
+    result.y = this->y - point.y;
+    return result;
+}
---
+++ Figure.h
@@ -0,0 +1,108 @@
+#ifndef _FIGURE_H_
+#define _FIGURE_H_
+
+#include <boost/optional.hpp>
+#include "FigureException.h"
+
+class Figure;
+class Square;
+class Circle;
+
+class IntersectVisitor
+{
+public:
+    virtual bool visit(const Square&) const = 0;
+    virtual bool visit(const Circle&) const = 0;
+};
+
+class SquareVisitor : public IntersectVisitor
+{
+public:
+    SquareVisitor(const Square&);
+    bool visit(const Square&) const; // for intersect(Square,Square)
+    bool visit(const Circle&) const; // for intersect(Circle,Square)
+private:
+    const Square& square_;
+};
+
+class CircleVisitor : public IntersectVisitor
+{
+public:
+    CircleVisitor(const Circle&);
+    bool visit(const Square&) const; // for intersect(Square,Circle)
+    bool visit(const Circle&) const; // for intersect(Circle,Circle)
+private:
+    const Circle& circle_;
+};
+
+struct Point
+{
+    int x;
+    int y;
+    Point operator-(const Point&) const;
+};
+
+class Figure
+{
+public:
+    Figure(const Point&);
+    virtual bool accept(IntersectVisitor&) const = 0;
+    /**
+     * Method checks whether current figure intersect given one.
+     * @param const Figure& Reference to figure to be intersected with.
+     * @return bool true if intersects, false otherwise.
+     */
+    virtual bool intersects(const Figure&) const = 0;
+    virtual double getDistance(const Figure&) const;
+protected:
+    Point centerPoint;
+};
+
+class Square : public Figure
+{
+public:
+    Square(const Point&, unsigned int);
+    bool accept(IntersectVisitor&) const;
+    double getEdgeLength() const;
+
+    /**
+     * Method checks whether current figure intersect given one.
+     * @param const Figure& Reference to figure to be intersected with.
+     * @return bool true if intersects, false otherwise.
+     */
+    bool intersects(const Figure&) const;
+
+    /**
+     * Method returns distance between two squares basis on only X axis.
+     * @param const Square& Reference to square to be compared with current.
+     * @return double distance (subtraction of X-axis of center points).
+     */
+    virtual double getDistanceX(const Square&) const;
+
+    /**
+     * Method returns distance between two squares basis on only Y axis.
+     * @param const Square& Reference to square to be compared with current.
+     * @return double distance (subtraction of Y-axis of center points).
+     */
+    virtual double getDistanceY(const Square&) const;
+private:
+    double edgeLength;
+};
+
+class Circle : public Figure
+{
+public:
+    Circle(const Point&, unsigned int);
+    bool accept(IntersectVisitor&) const;
+    double getRadius() const;
+    /**
+     * Method checks whether current figure intersect given one.
+     * @param const Figure& Reference to figure to be intersected with.
+     * @return bool true if intersects, false otherwise.
+     */
+    virtual bool intersects(const Figure&) const;
+private:
+    double radius_;
+};
+
+#endif
